0af3f9349239ff88a4c69fda2d6fc1eb474a9771
Paul <johnpauloco435@gmail.com>
2024-05-16 21:23:43 +0800

    featured creators scrolling upd

--- PixelPurse/assets/scripts/index.js
+++ PixelPurse/assets/scripts/index.js
@@ -1,7 +1,3 @@
 
 
-//login form popup
-document.getElementById("loginLink").addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent default link behavior
-    document.getElementById("loginPopup").style.display = "block"; // Show the login popup
-});
+
