7fb75015347e9b3d6271f1f637bf4ee48aa261d7
Paul <johnpauloco435@gmail.com>
2024-05-19 23:37:57 +0800

    js - login - nav functions

--- PixelPurse/assets/scripts/index.js
+++ PixelPurse/assets/scripts/index.js
@@ -1,3 +1,23 @@
 
 
 
+setTimeout(function() {
+    const container = document.getElementById('creatorsContainer');
+    container.style.animation = 'scrollRight 20s linear infinite'; // 17s = 25s (total duration) - 8s (initial delay)
+  }, 1); // 1000ms = 1s
+
+
+
+  document
+    .getElementById("loginLink")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      document.getElementById("loginPopup").style.display = "block"; // Show the login popup
+      document.getElementById("overlay").style.display = "block"; //dark overlay
+    });
+
+  //when user click outside the form
+  document.getElementById("overlay").addEventListener("click", function () {
+    document.getElementById("loginPopup").style.display = "none"; // Hide the login popup
+    document.getElementById("overlay").style.display = "none"; // Hide the overlay
+  });
---
+++ PixelPurse/assets/scripts/login.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.getElementById("loginForm");
+    const messageDiv = document.getElementById("message");
+
+    loginForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        // DUMMY USERS FOR DEMO PURPOSES - SINCE WE DON'T HAVE A DATABASE
+        const dummyUsers = [
+            { username: "juan_pedro", password: "juanpedro123" }, //Main user for demo
+            { username: "user2", password: "password2" },
+            { username: "user3", password: "password3" }
+        ];
+
+
+        // User creds checker oh yeah
+        const user = dummyUsers.find(user => user.username === username && user.password === password);
+
+        if (user) {
+            localStorage.setItem("isLoggedIn", "true");
+            localStorage.setItem("username", username);
+            window.location.href = "./pages/home.html";
+        } else {
+            messageDiv.textContent = "Invalid username or password.";
+        }
+    });
+});
